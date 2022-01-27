45a068ce67cab8e8d4d9df25bf7f3dd9939e1ed1
Zar21 <ivanperezfita@gmail.com>
2022-01-27 22:27:30 +0100

    Angular login added

---
+++ frontend/src/app/_services/user.service.ts
@@ -0,0 +1,13 @@
+﻿import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
+import { User } from '../_models';
+
+@Injectable({ providedIn: 'root' })
+export class UserService {
+    constructor(private http: HttpClient) { }
+
+    editUser() {
+        return this.http.put(`${environment.api}/users`, {});
+    }
+}
---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,27 @@
+﻿import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+
+import { appRoutingModule } from './app.routing';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home';
+import { LoginComponent } from './login';
+
+@NgModule({
+    imports: [
+        BrowserModule,
+        ReactiveFormsModule,
+        HttpClientModule,
+        appRoutingModule
+    ],
+    declarations: [
+        AppComponent,
+        HomeComponent,
+        LoginComponent,
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
+})
+export class AppModule { };
